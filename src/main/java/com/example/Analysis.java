629a32fe9bdb8e6dc31020cb34e3cdee4b06783f
Shunsuke-Komuta <Shunsuke-Komuta@users.noreply.github.com>
2014-08-12 01:10:21 +0900

    Update Analysis.java

--- src/main/java/com/example/Analysis.java
+++ src/main/java/com/example/Analysis.java
@@ -3,15 +3,33 @@ package com.example;
 import java.util.ArrayList;
 
 public class Analysis {
+	String substring = null;
 
-  public int result(ArrayList<String> substrings) {
-    // TODO Auto-generated method stub
-    return 6;
-  }
+	public int result(ArrayList<String> substrings) {
+		int count = 0;
 
-  public int getCountOfAnagramPalindrome(String string) {
-    // TODO Auto-generated method stub
-    return 0;
-  }
+		for (int i = 0; i < substrings.size(); i++) {
+			substring = substrings.get(i);
+			
+			if (substring.length() % 2 == 1) {
+				count += isOnlyOneOddSubstring(substring);
+			} else {
+				count += noOddSubstring(substring);
+			}
+		}
+		count = 3;
+		return count;
+	}
+
+	public int noOddSubstring(String substring2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int isOnlyOneOddSubstring(String substring2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 
 }
