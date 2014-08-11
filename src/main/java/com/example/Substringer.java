3f0835141d5d8550af726b57b1805054ee1127fa
Shunsuke-Komuta <Shunsuke-Komuta@users.noreply.github.com>
2014-08-11 23:44:28 +0900

    Update Substringer.java

--- src/main/java/com/example/Substringer.java
+++ src/main/java/com/example/Substringer.java
@@ -2,33 +2,31 @@ package com.example;
 
 import java.util.ArrayList;
 
-
 public class Substringer {
 
-  public ArrayList<String> createArrayOfSubstrings(String string) {
-    ArrayList<String> substrings = new ArrayList<String>();
+	public ArrayList<String> createArrayOfSubstrings(String string) {
+		ArrayList<String> substrings = new ArrayList<String>();
 
-    int j = string.length();
-    int beginIndex = 0;
-    int endIndex = 0;
+		int length = string.length();
+		int beginIndex = 0;
+		int endIndex = 0;
 
-    for (int i = 0; i < j; i++) {
-      beginIndex = 0;
-      endIndex = i;
-      for (i = 0; i < j; i++) {
+		for (int i = 0; i < length; i++) {
+			beginIndex = 0;
+			endIndex = i+1;
+			for (int k = 0; endIndex <= length; k++) {
+				if (endIndex == length) {
+					substrings.add(string.substring(beginIndex));
+					System.out.println(string.substring(beginIndex));
+				} else {
+					substrings.add(string.substring(beginIndex, endIndex));
+					System.out.println(string.substring(beginIndex, endIndex));
+				}
+				++beginIndex;
+				++endIndex;
+			}
+		}
+		return substrings;
 
-        if (endIndex == j) {
-          substrings.add(string.substring(beginIndex));
-          System.out.println(string.substring(beginIndex));
-        } else {
-          substrings.add(string.substring(beginIndex, endIndex));
-          System.out.println(string.substring(beginIndex, endIndex));
-        }
-        ++beginIndex;
-        ++endIndex;
-      }
-    }
-    return substrings;
-
-  }
+	}
 }
