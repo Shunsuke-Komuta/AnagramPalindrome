4606062626a026c11d41677c731ae34696b7e9bb
Shunsuke-Komuta <Shunsuke-Komuta@users.noreply.github.com>
2014-08-11 23:43:02 +0900

    Update SubstringerTest.java

--- src/test/java/com/example/SubstringerTest.java
+++ src/test/java/com/example/SubstringerTest.java
@@ -1,5 +1,10 @@
 package com.example;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
@@ -19,6 +24,16 @@ public class SubstringerTest {
   }
 
   @Test
+  public void test_文字列を引数で渡すとすべての部分文字列を格納したリストを返すメソッドを呼び出すpart0() {
+
+    substrings = substringer.createArrayOfSubstrings("ab");
+
+    assertEquals("a", substrings.get(0));
+    assertEquals("b", substrings.get(1));
+    assertEquals("ab", substrings.get(2));
+  }
+  
+  @Test
   public void test_文字列を引数で渡すとすべての部分文字列を格納したリストを返すメソッドを呼び出すpart1() {
 
     substrings = substringer.createArrayOfSubstrings("aaa");
@@ -34,18 +49,18 @@ public class SubstringerTest {
   @Test
   public void test_文字列を引数で渡すとすべての部分文字列を格納したリストを返すメソッドを呼び出すpart2() {
 
-    substringer.createArrayOfSubstrings("perl");
+    substrings = substringer.createArrayOfSubstrings("perl");
 
     assertEquals("p", substrings.get(0));
-    assertEquals("pe", substrings.get(1));
-    assertEquals("per", substrings.get(2));
-    assertEquals("perl", substrings.get(3));
-    assertEquals("e", substrings.get(4));
+    assertEquals("e", substrings.get(1));
+    assertEquals("r", substrings.get(2));
+    assertEquals("l", substrings.get(3));
+    assertEquals("pe", substrings.get(4));
     assertEquals("er", substrings.get(5));
-    assertEquals("erl", substrings.get(6));
-    assertEquals("r", substrings.get(7));
-    assertEquals("rl", substrings.get(8));
-    assertEquals("l", substrings.get(9));
+    assertEquals("rl", substrings.get(6));
+    assertEquals("per", substrings.get(7));
+    assertEquals("erl", substrings.get(8));
+    assertEquals("perl", substrings.get(9));
   }
 
 
