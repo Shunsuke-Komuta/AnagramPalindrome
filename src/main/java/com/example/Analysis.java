175e8eee96b51e2a6770f1810f64104bc2ba3ad1
俊介 <@Lenovo-PC>
2014-08-12 12:14:47 +0900

    文字列を構成する文字とその個数を返すメソッドを実装

--- src/main/java/com/example/Analysis.java
+++ src/main/java/com/example/Analysis.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Analysis {
   String substring = null;
@@ -10,27 +11,24 @@ public class Analysis {
 
     for (int i = 0; i < substrings.size(); i++) {
       substring = substrings.get(i);
-
-      if (substring.length() % 2 == 1) {
-        count += isOnlyOneOddSubstring(substring);
-      } else {
-        count += noOddSubstring(substring);
-      }
     }
     count = 3;
     return count;
   }
 
-  public int noOddSubstring(String substring2) {
-    // TODO Auto-generated method stub
-    return 0;
+  public HashMap<String, Integer> createOddStringsTabel(ArrayList<String> oddStrings) {
+    HashMap<String, Integer> oddTable = new HashMap<String, Integer>();
+
+    for (int i = 0; i < oddStrings.size(); i++) {
+      String string = oddStrings.get(i);
+      int k = 0;
+
+      if (oddTable.containsKey(string)) {
+        k = oddTable.get(string);
+      }
+      k++;
+      oddTable.put(string, k);
+    }
+    return oddTable;
   }
-
-  public int isOnlyOneOddSubstring(String substring2) {
-    // TODO Auto-generated method stub
-    return 0;
-  }
-
-
-
 }
--- src/test/java/com/example/AnalysisTest.java
+++ src/test/java/com/example/AnalysisTest.java
@@ -3,17 +3,24 @@ package com.example;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class AnalysisTest {
   ArrayList<String> substrings = null;
+  ArrayList<String> oddStrings = null;
+  ArrayList<String> evenStrings = null;
+
   Analysis analysis = null;
 
   @Before
   public void setUp() {
     substrings = new ArrayList<String>();
+    oddStrings = new ArrayList<String>();
+    evenStrings = new ArrayList<String>();
+
     analysis = new Analysis();
   }
 
@@ -28,21 +35,27 @@ public class AnalysisTest {
     assertEquals(3, actual);
   }
 
-  // @Test
-  // public void test_abの部分文字列が格納されたリストを引数で渡されるとアナグラム回文の結果を返す() {
-  // substrings.add("a");
-  // substrings.add("b");
-  // substrings.add("ab");
-  //
-  // int actual = analysis.result(substrings);
-  //
-  // assertEquals(2, actual);
-  // }
-
   @Test
-  public void test_a部分文字列が引数で渡されると() {
+  public void test_a_aが格納されたリストが渡されると文字とその個数を格納したものを返す() {
+    oddStrings.add("a");
+    oddStrings.add("a");
+    HashMap<String, Integer> oddTable = new HashMap<String, Integer>();
 
+    oddTable = analysis.createOddStringsTabel(oddStrings);
+
+    assertEquals(2, (int) oddTable.get("a"));
   }
 
+  @Test
+  public void test_a_bが格納されたリストが渡されると文字とその個数を格納したものを返す() {
+    oddStrings.add("a");
+    oddStrings.add("b");
+    HashMap<String, Integer> oddTable = new HashMap<String, Integer>();
+
+    oddTable = analysis.createOddStringsTabel(oddStrings);
+
+    assertEquals(1, (int) oddTable.get("a"));
+    assertEquals(1, (int) oddTable.get("b"));
+  }
 
 }
