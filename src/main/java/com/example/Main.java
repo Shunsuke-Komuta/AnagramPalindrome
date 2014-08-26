06515dc656537a107d0cfd1c0c9908824425f386
Takuo Doi <takuo.doi@iclound.com>
2014-08-26 09:38:49 +0900

    Changed the IO according to the expected format.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,24 +9,18 @@ public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("チェックする文字列の数：");
     int num = scanner.nextInt();
     String[] array = new String[num];
     int[] result = new int[num];
 
-    System.out.println("文字列を入力して下さい");
     for (int i = 0; i < num; i++) {
       array[i] = scanner.next();
     }
-
-    long start = System.currentTimeMillis();
-
+    
     result = executor.getResult(array);
 
     for (int i = 0; i < num; i++) {
       System.out.println(result[i]);
     }
-    long stop = System.currentTimeMillis();
-    System.out.println("実行にかかった時間は " + (stop - start) + " ミリ秒です。");
   }
 }
